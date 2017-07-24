9336b4bb423ec9d18ec7594d1214f9950084412a
Dane Petersen <thegreatsunra@gmail.com>
2017-07-24 12:24:59 -0500

    Add jest and stub unit test

---
+++ test/server/unit/specs/app.spec.js
@@ -0,0 +1,11 @@
+// const path = require('path')
+
+// const app = require(path.resolve(__dirname, '../../../../', 'src/server'))
+
+/* eslint-disable no-undef */
+describe('App', () => {
+  it('should compare true and true', () => {
+    expect(true).toEqual(true)
+  })
+})
+/* eslint-enable */
